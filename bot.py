7bb6ea13ac507d1279dc346a027a51777c5b56df
kristinavvon1303-art <kristina.vvon.1303@gmail.com>
2026-05-12 21:36:28 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -96,3 +96,17 @@ def fallback(message):
 
 
 bot.infinity_polling()
+
+import threading
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run_web():
+    app.run(host="0.0.0.0", port=10000)
+
+threading.Thread(target=run_web).start()
